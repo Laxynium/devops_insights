02eb63d967946f9eb0cc00a998bdc83e36b5a062
Laxynium <grzesiu15432@gmail.com>
2024-03-02 13:01:48 +0100

    feat: add dialyxir

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule DevopsInsights.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:phoenix, "~> 1.7.11"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
