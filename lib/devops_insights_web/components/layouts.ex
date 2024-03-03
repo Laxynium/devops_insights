26a4ecdc526efd926f70bbb7f09c69ae7b364125
Laxynium <grzesiu15432@gmail.com>
2024-03-03 14:29:53 +0100

    chore: remove auto generated ui

--- lib/devops_insights_web/components/layouts.ex
+++
@@ -1,5 +0,0 @@
-defmodule DevopsInsightsWeb.Layouts do
-  use DevopsInsightsWeb, :html
-
-  embed_templates "layouts/*"
-end
--- lib/devops_insights_web/controllers/page_html.ex
+++
@@ -1,5 +0,0 @@
-defmodule DevopsInsightsWeb.PageHTML do
-  use DevopsInsightsWeb, :html
-
-  embed_templates "page_html/*"
-end
