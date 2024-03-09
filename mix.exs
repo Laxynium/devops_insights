8001459b51e0ac205c8a8769019a958491dc0ec4
Laxynium <grzesiu15432@gmail.com>
2024-03-09 11:50:43 +0100

    refactor(#1): modularize events_ingestion, move fixtures

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule DevopsInsights.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
