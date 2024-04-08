2e20ce8f53373b557a026578f346956559c14392
Laxynium <grzesiu15432@gmail.com>
2024-04-08 23:51:11 +0200

    feat(#2): improve error handling when there is already a root commit

--- lib/devops_insights/events_ingestion/commits/commits_gateway.ex
+++ lib/devops_insights/events_ingestion/commits/commits_gateway.ex
@@ -1,22 +1,35 @@
 defmodule DevopsInsights.EventsIngestion.Commits.CommitsGateway do
+  alias Ecto.Changeset
   alias DevopsInsightsWeb.Endpoint
   alias DevopsInsights.Repo
   alias DevopsInsights.EventsIngestion.Commits.Commit
   import Ecto.Query, only: [from: 2]
 
   def create_root_commit(attr \\ %{}) do
-    service_name = Map.get(attr, "service_name")
+    commit_changeset = %Commit{} |> Commit.changeset(attr)
 
-    any_root =
-      Repo.exists?(
-        from c in "commits",
-          where: is_nil(c.parent_id) and c.service_name == ^service_name
-      )
+    with %Ecto.Changeset{valid?: true} <- commit_changeset do
+      service_name = Changeset.fetch_field!(commit_changeset, :service_name)
 
-    if(any_root == true) do
-      {:error, "There is already a root commit"}
+      any_root =
+        Repo.exists?(
+          from c in "commits",
+            where: is_nil(c.parent_id) and c.service_name == ^service_name
+        )
+
+      if(any_root == true) do
+        {:error,
+         commit_changeset
+         |> Changeset.add_error(
+           :commit,
+           "there is already a root commit for this service_name"
+         )}
+        |> IO.inspect()
+      else
+        handle_commit_insertion(attr)
+      end
     else
-      handle_commit_insertion(attr)
+      _ -> {:error, commit_changeset} |> IO.inspect()
     end
   end
 
