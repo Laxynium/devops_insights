5a7f80415c272221cca08d4e5738e9b49ee46abc
Laxynium <grzesiu15432@gmail.com>
2024-04-19 21:25:31 +0200

    feat(#2): add initial view for lead time for changes

--- lib/devops_insights/lead_time_for_changes/live/index.ex
+++ lib/devops_insights/lead_time_for_changes/live/index.ex
@@ -1,9 +1,31 @@
 defmodule DevopsInsights.LeadTimeForChanges.Live.Index do
+  alias DevopsInsights.EventsIngestion.EventsFilter
   use DevopsInsightsWeb, :live_view
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    search_filters = %EventsFilter{
+      start_date: Date.utc_today() |> Date.add(-13),
+      end_date: Date.utc_today(),
+      interval: 1
+    }
+
+    available_dimentions = %{
+      serviceName: %{displayName: "Service Name", values: MapSet.new([nil, "app-1", "app-2"])}
+    }
+
+    dimentions_filter =
+      Map.keys(available_dimentions)
+      |> Enum.reduce(%{}, fn x, acc -> Map.put(acc, x, nil) end)
+
+    intervals_to_choose = ["1 day": 1, "1 week": 7, "2 weeks": 14, "1 month": 30]
+
+    {:ok,
+     socket
+     |> assign(:available_dimentions, available_dimentions)
+     |> assign(:intervals_to_choose, intervals_to_choose)
+     |> assign(:dimentions_filter, dimentions_filter)
+     |> assign(search_filters |> EventsFilter.to_map())}
   end
 
   @impl true
