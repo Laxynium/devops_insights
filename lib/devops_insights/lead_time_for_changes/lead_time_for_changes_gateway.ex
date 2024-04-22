07a23cb93ae3718f549a50ceb9b99e313d43d713
Laxynium <grzesiu15432@gmail.com>
2024-04-22 22:37:08 +0200

    feat(#2): add fake lead time for changes implementation

--- lib/devops_insights/lead_time_for_changes/lead_time_for_changes_gateway.ex
+++ lib/devops_insights/lead_time_for_changes/lead_time_for_changes_gateway.ex
@@ -1,4 +1,6 @@
 defmodule DevopsInsights.LeadTimeForChanges.LeadTimeForChangesGateway do
+  alias DevopsInsights.EventsIngestion.Commits.Commit
+  alias DevopsInsights.EventsIngestion.Commits
   alias DevopsInsights.EventsIngestion.Deployments.Deployment
   alias DevopsInsights.Repo
   alias DevopsInsights.EventsIngestion.IntervalFilter
@@ -7,5 +9,9 @@ defmodule DevopsInsights.LeadTimeForChanges.LeadTimeForChangesGateway do
         %IntervalFilter{start_date: start_date, end_date: end_date, interval: interval},
         dimensions \\ []
       ) do
+    deployments = Repo.all(Deployment)
+    commits = Repo.all(Commit)
+
+    {:ok, 15}
   end
 end
--- test/devops_insights/lead_time_for_changes/lead_time_for_changes_metric_test.exs
+++ test/devops_insights/lead_time_for_changes/lead_time_for_changes_metric_test.exs
@@ -1,2 +1,48 @@
 defmodule DevopsInsights.LeadTimeForChanges.LeadTimeForChangesMetricTest do
+  alias DevopsInsights.EventsIngestion.IntervalFilter
+  alias DevopsInsights.LeadTimeForChanges.LeadTimeForChangesGateway
+  alias DevopsInsights.EventsIngestion.Deployments.DeploymentsGateway
+  alias DevopsInsights.EventsIngestion.Commits.CommitsGateway
+  alias DevopsInsights.EventsIngestion
+  use DevopsInsights.DataCase
+
+  test "single deployment with commits before and after" do
+    {:ok, _} =
+      CommitsGateway.create_root_commit(%{
+        "commit_id" => "1",
+        "service_name" => "app-1",
+        "timestamp" => "2024-04-04T19:00:00Z"
+      })
+
+    {:ok, _} =
+      CommitsGateway.create_commit(%{
+        "commit_id" => "2",
+        "service_name" => "app-1",
+        "timestamp" => "2024-04-04T19:10:00Z",
+        "parent_id" => "1"
+      })
+
+    {:ok, _} =
+      CommitsGateway.create_commit(%{
+        "commit_id" => "3",
+        "service_name" => "app-1",
+        "timestamp" => "2024-04-04T19:20:00Z",
+        "parent_id" => "2"
+      })
+
+    {:ok, _} =
+      DeploymentsGateway.create_deployment(%{
+        "commit_id" => "2",
+        "serviceName" => "app-1",
+        "environment" => "prod",
+        "timestamp" => "2024-04-04T19:15:00Z"
+      })
+
+    {:ok, 15} =
+      LeadTimeForChangesGateway.get_lead_time_for_changes_metric(
+        %IntervalFilter{start_date: ~D[2024-01-01], end_date: ~D[2025-01-01], interval: 365},
+        service_name: "app-1",
+        environment: "prod"
+      )
+  end
 end
