f1ca45bca767f4217b71a566e97d853bc43ac4cb
Grzegorz Gruszczyk <grzesiu15432@gmail.com>
2024-04-28 06:36:21 +0000

    refactor(#2): make tests more concise

--- test/devops_insights/lead_time_for_changes/lead_time_for_changes_metric_test.exs
+++ test/devops_insights/lead_time_for_changes/lead_time_for_changes_metric_test.exs
@@ -9,199 +9,105 @@ defmodule DevopsInsights.LeadTimeForChanges.LeadTimeForChangesMetricTest do
   use DevopsInsights.DataCase
 
   test "no deployments single commit" do
-    assert {:ok, _} =
-             CommitsGateway.create_root_commit(%{
-               "commit_id" => "1",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:00:00Z"
-             })
+    [
+      %{type: :commit, commit_id: "1", parent_id: nil, timestamp: "2024-04-04T19:00:00Z"}
+    ]
+    |> apply_events()
 
-    assert {:error, "No deployments yet"} =
-             LeadTimeForChangesGateway.get_lead_time_for_changes_metric(
-               %IntervalFilter{
-                 start_date: ~D[2024-01-01],
-                 end_date: ~D[2025-01-01],
-                 interval: 365
-               },
-               service_name: "app-1",
-               environment: "prod"
-             )
+    assert {:error, "No deployments yet"} = get_lead_time_for_changes_metric()
   end
 
   test "single deployment single commit" do
-    assert {:ok, _} =
-             CommitsGateway.create_root_commit(%{
-               "commit_id" => "1",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:00:00Z"
-             })
+    [
+      %{type: :commit, commit_id: "1", parent_id: nil, timestamp: "2024-04-04T19:00:00Z"},
+      %{type: :deploy, commit_id: "1", timestamp: "2024-04-04T19:10:00Z"}
+    ]
+    |> apply_events()
 
-    assert {:ok, _} =
-             DeploymentsGateway.create_deployment(%{
-               "commit_id" => "1",
-               "serviceName" => "app-1",
-               "environment" => "prod",
-               "timestamp" => "2024-04-04T19:10:00Z"
-             })
-
-    assert {:ok, 600} =
-             LeadTimeForChangesGateway.get_lead_time_for_changes_metric(
-               %IntervalFilter{
-                 start_date: ~D[2024-01-01],
-                 end_date: ~D[2025-01-01],
-                 interval: 365
-               },
-               service_name: "app-1",
-               environment: "prod"
-             )
+    assert {:ok, 600} = get_lead_time_for_changes_metric()
   end
 
   test "each commit is deployed" do
-    assert {:ok, _} =
-             CommitsGateway.create_root_commit(%{
-               "commit_id" => "1",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:00:00Z"
-             })
+    [
+      %{type: :commit, commit_id: "1", parent_id: nil, timestamp: "2024-04-04T19:00:00Z"},
+      %{type: :commit, commit_id: "2", parent_id: "1", timestamp: "2024-04-04T19:10:00Z"},
+      %{type: :deploy, commit_id: "1", timestamp: "2024-04-04T19:12:00Z"},
+      %{type: :deploy, commit_id: "2", timestamp: "2024-04-04T19:20:00Z"}
+    ]
+    |> apply_events()
 
-    assert {:ok, _} =
-             CommitsGateway.create_commit(%{
-               "commit_id" => "2",
-               "parent_id" => "1",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:10:00Z"
-             })
-
-    assert {:ok, _} =
-             DeploymentsGateway.create_deployment(%{
-               "commit_id" => "1",
-               "serviceName" => "app-1",
-               "environment" => "prod",
-               "timestamp" => "2024-04-04T19:12:00Z"
-             })
-
-    assert {:ok, _} =
-             DeploymentsGateway.create_deployment(%{
-               "commit_id" => "2",
-               "serviceName" => "app-1",
-               "environment" => "prod",
-               "timestamp" => "2024-04-04T19:20:00Z"
-             })
-
-    assert {:ok, 660} =
-             LeadTimeForChangesGateway.get_lead_time_for_changes_metric(
-               %IntervalFilter{
-                 start_date: ~D[2024-01-01],
-                 end_date: ~D[2025-01-01],
-                 interval: 365
-               },
-               service_name: "app-1",
-               environment: "prod"
-             )
+    assert {:ok, 660} = get_lead_time_for_changes_metric()
   end
 
   test "commits gap between deployments" do
-    assert {:ok, _} =
-             CommitsGateway.create_root_commit(%{
-               "commit_id" => "1",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:00:00Z"
-             })
+    [
+      %{type: :commit, commit_id: "1", parent_id: nil, timestamp: "2024-04-04T19:00:00Z"},
+      %{type: :commit, commit_id: "2", parent_id: "1", timestamp: "2024-04-04T19:10:00Z"},
+      %{type: :deploy, commit_id: "2", timestamp: "2024-04-04T19:20:00Z"}
+    ]
+    |> apply_events()
 
-    assert {:ok, _} =
-             CommitsGateway.create_commit(%{
-               "commit_id" => "2",
-               "parent_id" => "1",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:10:00Z"
-             })
-
-    assert {:ok, _} =
-             DeploymentsGateway.create_deployment(%{
-               "commit_id" => "2",
-               "serviceName" => "app-1",
-               "environment" => "prod",
-               "timestamp" => "2024-04-04T19:20:00Z"
-             })
-
-    assert {:ok, 900} =
-             LeadTimeForChangesGateway.get_lead_time_for_changes_metric(
-               %IntervalFilter{
-                 start_date: ~D[2024-01-01],
-                 end_date: ~D[2025-01-01],
-                 interval: 365
-               },
-               service_name: "app-1",
-               environment: "prod"
-             )
+    assert {:ok, 900} = get_lead_time_for_changes_metric()
   end
 
   test "deploy each second commit" do
-    assert {:ok, _} =
-             CommitsGateway.create_root_commit(%{
-               "commit_id" => "1",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:00:00Z"
-             })
+    [
+      %{type: :commit, commit_id: "1", parent_id: nil, timestamp: "2024-04-04T19:00:00Z"},
+      %{type: :commit, commit_id: "2", parent_id: "1", timestamp: "2024-04-04T19:10:00Z"},
+      %{type: :commit, commit_id: "3", parent_id: "2", timestamp: "2024-04-04T19:15:00Z"},
+      %{type: :deploy, commit_id: "2", timestamp: "2024-04-04T19:20:00Z"},
+      %{type: :commit, commit_id: "4", parent_id: "3", timestamp: "2024-04-04T19:30:00Z"},
+      %{type: :commit, commit_id: "5", parent_id: "4", timestamp: "2024-04-04T19:35:00Z"},
+      %{type: :deploy, commit_id: "5", timestamp: "2024-04-04T19:40:00Z"}
+    ]
+    |> apply_events()
 
-    assert {:ok, _} =
-             CommitsGateway.create_commit(%{
-               "commit_id" => "2",
-               "parent_id" => "1",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:10:00Z"
-             })
+    assert {:ok, 600} = get_lead_time_for_changes_metric()
+  end
 
-    assert {:ok, _} =
-             CommitsGateway.create_commit(%{
-               "commit_id" => "3",
-               "parent_id" => "2",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:15:00Z"
-             })
+  defp apply_events(events) do
+    events |> Enum.each(&apply_event/1)
+  end
 
-    assert {:ok, _} =
-             DeploymentsGateway.create_deployment(%{
-               "commit_id" => "2",
-               "serviceName" => "app-1",
-               "environment" => "prod",
-               "timestamp" => "2024-04-04T19:20:00Z"
-             })
+  defp apply_event(%{type: :commit} = commit) do
+    if(commit.parent_id == nil) do
+      assert {:ok, _} =
+               CommitsGateway.create_root_commit(%{
+                 "commit_id" => commit.commit_id,
+                 "service_name" => "app-1",
+                 "timestamp" => commit.timestamp
+               })
+    else
+      assert {:ok, _} =
+               CommitsGateway.create_commit(%{
+                 "commit_id" => commit.commit_id,
+                 "parent_id" => commit.parent_id,
+                 "service_name" => "app-1",
+                 "timestamp" => commit.timestamp
+               })
+    end
+  end
 
-    assert {:ok, _} =
-             CommitsGateway.create_commit(%{
-               "commit_id" => "4",
-               "parent_id" => "3",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:30:00Z"
-             })
-
-    assert {:ok, _} =
-             CommitsGateway.create_commit(%{
-               "commit_id" => "5",
-               "parent_id" => "4",
-               "service_name" => "app-1",
-               "timestamp" => "2024-04-04T19:35:00Z"
-             })
-
+  defp apply_event(%{type: :deploy} = deploy) do
     assert {:ok, _} =
              DeploymentsGateway.create_deployment(%{
-               "commit_id" => "5",
+               "commit_id" => deploy.commit_id,
                "serviceName" => "app-1",
                "environment" => "prod",
-               "timestamp" => "2024-04-04T19:40:00Z"
+               "timestamp" => deploy.timestamp
              })
+  end
 
-    assert {:ok, 600} =
-             LeadTimeForChangesGateway.get_lead_time_for_changes_metric(
-               %IntervalFilter{
-                 start_date: ~D[2024-01-01],
-                 end_date: ~D[2025-01-01],
-                 interval: 365
-               },
-               service_name: "app-1",
-               environment: "prod"
-             )
+  defp get_lead_time_for_changes_metric() do
+    LeadTimeForChangesGateway.get_lead_time_for_changes_metric(
+      %IntervalFilter{
+        start_date: ~D[2020-01-01],
+        end_date: ~D[2030-01-01],
+        interval: 365 * 10
+      },
+      service_name: "app-1",
+      environment: "prod"
+    )
   end
 end
 
